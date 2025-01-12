fea9489fdd3ad634eb86f6b4f52b95b77d6b4d3f
Yuskus <yuskus@yandex.ru>
2025-01-12 19:48:34 +0300

    Start work on weather app

--- src/App.vue
+++ src/App.vue
@@ -1,108 +1,66 @@
 <script>
-import User from './components/User.vue'
 
-export default {
-  components: { User },
-  data() {
-    return {
-      title: 'Name of this project',
-      info: 'I have some information!',
-      error: '',
-      userName: '',
-      userPass: '',
-      userMail: '',
-      users: [] 
-    }
-  },
-  methods: {
-    send() {
-      if (this.userName == '' || this.userPass == '' || this.userMail == '') {
-        this.error = 'Fill in all fields!';
-      }
-      else {
-        this.error = '';
-        this.users.push({
-          name: this.userName,
-          pass: this.userPass,
-          mail: this.userMail
-        });
-      }
-    },
-    deleteUser(index) {
-      this.users.splice(index, 1);
-    }
-  }
-}
 </script>
 
 <template>
-  <div class="app">
-    <h1>{{ title }}</h1>
-    <p>{{ info }}</p>
-
-    <div class="block-input">
-      <input type="text" v-model="userName" placeholder="Name" class="input"><br>
-      <input type="password" v-model="userPass" placeholder="Password" class="input"><br>
-      <input type="email" v-model="userMail" placeholder="Email" class="input"><br>
-
-      <button type="button" @click="send()" class="input">Send</button>
-
-      <p style="color: red">{{ error }}</p>
-    </div>
-
-    <p v-if="users.length == 0">"users" list is empty!</p>
-    <p v-else>"users" list has these elements:</p>
-
-    <User v-for="(el, index) in users" :key="index" :user="el" :number="index+1" :deleteUser="deleteUser" />
+  <div class="wrap">
+    <h1>Weather app</h1>
+    <h3>You can find out the weather in the city.</h3>
+    <input type="text" placeholder="Input your city" />
+    <button>Get the weather</button>
   </div>
 </template>
 
 <style scoped>
-* {
-  margin: 0;
-  padding: 0;
+.wrap {
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  padding: 20px;
+  background: #1f0f24;
+  text-align: center;
+  color: rgb(255, 253, 216);
 }
 
-.block-input {
-  background-color: rgb(241, 241, 255);
-  border-width: 1px;
-  border-radius: 5px;
-  padding: 15px;
+.wrap h1 {
+  margin-top: 50px;
+  font-family: Helvetica, Arial, Trebuchet, Tahoma, Verdana;
+  font-weight: 100;
 }
 
-.app {
-  background-color: rgb(218, 218, 231);
-  padding: 10px;
-  border-radius: 5px;
+.wrap h3 {
+  margin-top: 20px;
+  font-family: Helvetica, Arial, Trebuchet, Tahoma, Verdana;
+  font-weight: 100;
 }
 
-button {
-  min-width: 70px;
-  background-color: rgb(216, 234, 255);
-  border-color: rgb(61, 99, 145)
+.wrap input {
+  margin-top: 30px;
+  background: transparent;
+  border: 0px;
+  border-bottom: 2px solid #110813;
+  color: #fcfcfc;
+  font-size: 14px;
+  padding: 5px 8px;
+  outline: none;
 }
 
-button:hover {
-  background-color: rgb(148, 195, 248);
+.wrap input:focus {
+  border-bottom-color: #6e2d7d;
 }
 
-.input {
-  margin: 10px 0px 5px 0px;
-  padding: 7px;
-  border-width: 1px;
-  border-radius: 5px;
+.wrap button {
+  background: #e3bc4b;
+  color: #fff;
+  border-radius: 10px;
+  border: 2px solid #b99935;
+  padding: 10px 15px;
+  margin-left: 20px;
+  cursor: pointer;
+  transition: transform 500ms ease;
 }
 
-h1 {
-  padding: 15px 0px;
-  color: rgb(58, 127, 216);
-  font-weight: 300;
-  font-family: Roboto, Arial, Helvetica, sans-serif, system-ui;
-}
-
-p {
-  color: rgb(43, 37, 37);
-  font-family: Roboto, Arial, Helvetica, sans-serif, system-ui;
-  padding: 5px 0px;
+.wrap button:hover {
+  transform: scale(1.1) translateY(-5px);
 }
 </style>
