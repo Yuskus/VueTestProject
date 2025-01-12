15275154f097fa0040695873fa944184fd34936a
Yuskus <yuskus@yandex.ru>
2025-01-12 13:41:51 +0300

    Learn v-model, v-for, v-if

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,26 @@ export default {
     return {
       title: 'Name of this project',
       info: 'I have some information!',
+      error: '',
+      userName: '',
+      userPass: '',
+      userMail: '',
+      users: [] 
     }
   },
   methods: {
-    changeText() {
-      this.title = 'Tutorial';
+    send() {
+      if (this.userName == '' || this.userPass == '' || this.userMail == '') {
+        this.error = 'Fill in all fields!';
+      }
+      else {
+        this.error = '';
+        this.users.push({
+          name: this.userName,
+          pass: this.userPass,
+          mail: this.userMail
+        });
+      }
     }
   }
 }
@@ -18,7 +33,13 @@ export default {
   <div class="app">
     <h1 class="text-roboto">{{ title }}</h1>
     <p class="text-roboto">{{ info }}</p>
-    <button type="button" @click="changeText()">Кликни здесь!</button>
+    <input type="text" v-model="userName" placeholder="Name" class="input"><br>
+    <input type="password" v-model="userPass" placeholder="Password" class="input"><br>
+    <input type="email" v-model="userMail" placeholder="Email" class="input"><br>
+    <button type="button" @click="send()" class="input">Send</button>
+    <p class="text-roboto" style="color: red">{{ error }}</p>
+    <p class="text-roboto" v-if="users.length == 0">"users" is empty!</p>
+    <p class="text-roboto" v-for="(el, index) in users" :key="index">{{ index+1 }}. {{ el.name }}: [ {{ el.mail }}, <u>{{ el.pass }}</u> ]</p>
   </div>
 </template>
 
@@ -36,11 +57,24 @@ export default {
 
 .text-roboto {
   font-family: Roboto, Arial, Helvetica, sans-serif, system-ui;
+  padding: 5px 0px;
 }
 
 button {
-  margin: 20px 0px 10px 0px;
-  padding: 5px;
+  min-width: 70px;
+  background-color: rgb(216, 234, 255);
+  border-color: rgb(61, 99, 145)
+}
+
+button:hover {
+  background-color: rgb(148, 195, 248);
+}
+
+.input {
+  margin: 10px 0px 5px 0px;
+  padding: 7px;
+  border-width: 1px;
+  border-radius: 5px;
 }
 
 h1 {
